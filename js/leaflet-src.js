30e9bfae7051a2aa02d6ba1dc23c57d1a2929925
bulklc <31697679+bulklc@users.noreply.github.com>
2017-09-14 13:43:52 +0530

    Update leaflet-src.js

--- js/leaflet-src.js
+++ js/leaflet-src.js
@@ -8731,21 +8731,6 @@ function geoJSON(geojson, options) {
 // Backward compatibility.
 var geoJson = geoJSON;
 
-/*
- * @class ImageOverlay
- * @aka L.ImageOverlay
- * @inherits Interactive layer
- *
- * Used to load and display a single image over specific bounds of the map. Extends `Layer`.
- *
- * @example
- *
- * ```js
- * var imageUrl = 'http://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg',
- * 	imageBounds = [[40.712216, -74.22655], [40.773941, -74.12544]];
- * L.imageOverlay(imageUrl, imageBounds).addTo(map);
- * ```
- */
 
 var ImageOverlay = Layer.extend({
 
